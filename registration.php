8e3c6611786cc0dad07c8c931a423b3bf3c9b547
showvik_98 <showvik.saha01@gmail.com>
2021-04-25 16:11:09 +0600

    registration css

---
+++ registration.php
@@ -0,0 +1,27 @@
+
+<style>
+	.registrationDiv
+    {
+		width:fit-content;
+	    padding: 60px;
+	    height:fit-content;
+	    margin:auto;
+	    margin-top: 2%;
+	    background-color: #78c2c1
+	}
+	.fields
+    {
+		border-width:5px;  
+        border-style:double;
+		height: 40px;
+	}
+	.button1
+    {
+	  background-color: #4CAF50;
+	  color: black;
+	  border-width:5px;  
+      border-style:double;
+	  
+	}	
+	
+</style>
